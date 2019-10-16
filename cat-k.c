9cd8bea460b3d8001a6faadd31f10c75ebc096e6
marioruizcommx <aonce.edupen@gmail.com>
2019-10-16 13:03:33 -0500

    Update cat-k.c

--- cat-k.c
+++ cat-k.c
@@ -1,11 +1,11 @@
-/* Sweep
-  Write by Atlanta Algorithm <http://atlantaalgorithm.com>
-  by Mario Ruiz
-*/
 
-#include <Servo.h>
+#include <Servo.h>	
 
 //Declaracion de Servomotores
+
+
+long dis;
+long tiem;
 
 Servo sv_cuello;
 Servo sv_piernaDerecha;
@@ -22,13 +22,13 @@ int salida;
 //Declaracion de funciones
 
 void f_pruebaComponentes();
+void f_caminaPanza();
+void f_caminaSigiloso();
 
-int pos = 0;
-const int pinecho = 12;
-const int pintrigger = 13;
-unsigned int tiempo, distancia;
 
 void setup() {
+  
+  Serial.begin(9600);//iniciailzamos la comunicación
   sv_cuello.attach(2);
   sv_piernaIzquierda.attach(4);
   sv_piernaDerecha.attach(5);
@@ -38,63 +38,51 @@ void setup() {
   sv_rodillaDerecha.attach(9);
   sv_codoIzquierdo.attach(10);
   sv_codoDerecho.attach(11);
-  pinMode(pinecho, INPUT);
-  pinMode(pintrigger, OUTPUT);
+  
+pinMode(13, OUTPUT); //salida del pulso generado por el sensor ultrasónico
+pinMode(12, INPUT);//entrada del pulso generado por el sensor ultrasónico
+
 }
 
 //////////////////////////////
 
 void loop() {
-  Serial.begin(9600);
-  f_pruebaComponentes();
+m_inicio_operativo();
+
 }
 
 //////////////////////////////
 
 void m_inicio_operativo() {
-
-          for (pos = 1; pos <= 5; pos += 1) {
-            Serial.println("************************ Encendiendo Modulos ************************");
-            delayMicroseconds(600);
-          }
-  // ENVIAR PULSO DE DISPARO EN EL PIN "TRIGGER"
-  digitalWrite(pintrigger, LOW);
-  delayMicroseconds(2);
-  digitalWrite(pintrigger, HIGH);
-  // EL PULSO DURA AL MENOS 10 uS EN ESTADO ALTO
-  delayMicroseconds(10);
-  digitalWrite(pintrigger, LOW);
-  // MEDIR EL TIEMPO EN ESTADO ALTO DEL PIN "ECHO" EL PULSO ES PROPORCIONAL A LA DISTANCIA MEDIDA
-  tiempo = pulseIn(pinecho, HIGH);
-  // LA VELOCIDAD DEL SONIDO ES DE 340 M/S O 29 MICROSEGUNDOS POR CENTIMETRO
-  // DIVIDIMOS EL TIEMPO DEL PULSO ENTRE 58, TIEMPO QUE TARDA RECORRER IDA Y VUELTA UN CENTIMETRO LA ONDA SONORA
-  distancia = tiempo / 58;
-
-  if (distancia >= 15) {
-          for (pos = 1; pos <= 5; pos += 1) {
-            Serial.println("+++++++++++++++++++++++ muevete +++++++++++++++++++++++");
-            delayMicroseconds(500);
-          }
-      Serial.println(distancia);
-      Serial.println(" cm");
-      delayMicroseconds(500);
-      f_pruebaComponentes();
-   } else {
-          for (pos = 1; pos <= 5; pos += 1) {
-            Serial.println("----------------------- alto -----------------------");
-            delay(500);
-          }
-      delay(500);
-  }
-  delayMicroseconds(200);
+  digitalWrite(13,LOW);//recibimiento del pulso.
+delayMicroseconds(5);
+digitalWrite(13, HIGH);//envió del pulso.
+delayMicroseconds(10);
+tiem=pulseIn(12, HIGH);//fórmula para medir el pulso entrante.
+dis= long(0.017*tiem);//fórmula para calcular la distancia del sensor ultrasónico.
+if(dis>30){ //comparativo para la alarma se ingresa la distancia en la que encenderá o apagara.
+f_caminaPanza();
+}
+else
+{
+f_posicionZero();
+}
+Serial.println("LA DISTANCIA MEDIDA ES:");
+Serial.println(dis);
+Serial.println("cm");
+delay(500);
 }
 
 //////////////////////////////
 
 void f_pruebaComponentes() {
-    //posicion cero
+
+f_posicionZero;
     
-    /*
+    }
+    
+void f_posicionZero() {
+
     sv_piernaDerecha.write(90);
     sv_piernaIzquierda.write(90);
     sv_hombroIzquierdo.write(90);
@@ -104,56 +92,11 @@ void f_pruebaComponentes() {
     sv_codoIzquierdo.write(0);
     sv_codoDerecho.write(180);
     
-        sv_piernaDerecha.write(45);
-    sv_piernaIzquierda.write(135);
-    sv_hombroIzquierdo.write(135);
-    sv_hombroDerecho.write(45);
-    delay(500);
-    sv_piernaDerecha.write(135);
-    sv_piernaIzquierda.write(45);
-    sv_hombroIzquierdo.write(45);
-    sv_hombroDerecho.write(135);
-    delay(500);
-*/
-
-    sv_piernaDerecha.write(45);
-    sv_piernaIzquierda.write(45);
-    sv_hombroIzquierdo.write(45);
-    sv_hombroDerecho.write(45);
-
-
-    sv_piernaDerecha.write(135);
-    sv_piernaIzquierda.write(135);
-    sv_hombroIzquierdo.write(135);
-    sv_hombroDerecho.write(135);
-
-
     
-    
-    /*
-    for (pos = 1; pos <= 5; pos += 1) {
-    sv_piernaDerecha.write(45);
-    sv_piernaIzquierda.write(45);
-    sv_hombroIzquierdo.write(45);
-    sv_hombroDerecho.write(45);
-    sv_rodillaIzquierda.write(0);
-    sv_rodillaDerecha.write(180);
-    sv_codoIzquierdo.write(0);
-    sv_codoDerecho.write(180);
+}
 
-    sv_piernaDerecha.write(135);
-    sv_piernaIzquierda.write(135);
-    sv_hombroIzquierdo.write(135);
-    sv_hombroDerecho.write(135);
-    sv_rodillaIzquierda.write(0);
-    sv_rodillaDerecha.write(180);
-    sv_codoIzquierdo.write(0);
-    sv_codoDerecho.write(180);
-    }
-    */
-    
-    /*
-    for (pos = 1; pos <= 5; pos += 1) {
+void f_caminaSigiloso(){
+    for (int pos = 1; pos <= 5; pos += 1) {
         
     sv_piernaDerecha.write(135);
     sv_hombroDerecho.write(45);
@@ -192,6 +135,29 @@ void f_pruebaComponentes() {
     sv_rodillaIzquierda.write(0);
     delay(500);
     }
-*/
 
 }
+    
+void f_caminaPanza() {
+  
+    for (int pos = 1; pos <= 4; pos += 1) {
+    sv_rodillaIzquierda.write(0);
+    sv_rodillaDerecha.write(180);
+    sv_codoIzquierdo.write(0);
+    sv_codoDerecho.write(180);
+    delay(400);
+    sv_piernaDerecha.write(45);
+    sv_piernaIzquierda.write(135);
+    sv_hombroIzquierdo.write(45);
+    sv_hombroDerecho.write(135); 
+    delay(400);
+    sv_piernaDerecha.write(135);
+    sv_piernaIzquierda.write(45);
+    sv_hombroIzquierdo.write(135);
+    sv_hombroDerecho.write(45);
+    delay(400);
+    }
+    
+}
+
+
